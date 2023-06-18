43d48b55d14e04c39d276b2afef5002837feb94e
Darshna <darshnakhatri989@gmail.com>
2023-06-18 14:09:02 +0530

    submitting code-Sun Jun 18 2023 14:09:00 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
 import React from "react";
 import './../styles/App.css';
+import Content from "./Content";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <Content header={"Welcome to my website"} children = {"This is the content of my website."} footer={"© 2023 My Website. All rights reserved."}/>
+        
     </div>
   )
 }
